c459338047d62b26bfb25740e092df7b4843b52b
Dossman-thomas <tdossman.development@gmail.com>
2025-09-08 12:39:45 -0400

    landing-page wireframe complete

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,10 +1,12 @@
-
+import Button from 'react-bootstrap/Button';
 
 export default function WelcomePage () {
 
     return (
         <div>
             <h1>Welcome to QuizCard!</h1>
+            <h4>Ready to study, buddy?</h4>
+            <Button>Let's Go!</Button>
         </div>
     );
 }
