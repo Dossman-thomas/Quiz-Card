f21732be51e3a0108693d30ac883266bed39cc52
Dossman-thomas <tdossman.development@gmail.com>
2025-09-08 12:31:08 -0400

    router set up and welcome page initialized

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,33 @@
 // import react
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // import Bootstrap JS
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+// import react router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // import css
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
-
 
 // import pages
 import App from './App.jsx'
+import WelcomePage from './pages/welcome.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      { index: true, 
+        element: <WelcomePage /> },
+      // { path: 'manager', element: <ManagerPage /> },
+      // { path: 'study', element: <StudyPage /> },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,10 @@
+
+
+export default function WelcomePage () {
+
+    return (
+        <div>
+            <h1>Welcome to QuizCard!</h1>
+        </div>
+    );
+}
