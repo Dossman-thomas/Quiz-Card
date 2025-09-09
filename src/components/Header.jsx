4d78c879b094d589d0ae5b250c38c1bbb10e7342
Dossman-thomas <tdossman.development@gmail.com>
2025-09-09 14:44:00 -0400

    added basic style and NavLink active style to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Navbar collapseOnSelect expand="lg" fixed="top" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="/">Quiz Card</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/manage-cards">Manage Cards</Nav.Link>
-            <Nav.Link href="/study">Study Mode</Nav.Link>
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/manage-cards">Manage Cards</Nav.Link>
+            <Nav.Link as={NavLink} to="/study">Study Mode</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
