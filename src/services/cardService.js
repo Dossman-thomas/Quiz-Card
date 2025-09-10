a28ae699336e966848a904f34acea8600d015934
Dossman-thomas <tdossman.development@gmail.com>
2025-09-10 14:51:51 -0400

    initialized card service functions file

---
+++ src/services/cardService.js
@@ -0,0 +1,17 @@
+// Crud Service functions for Getting, Creating, Updating, and Deleting flashcards
+
+const getCards = async () => {
+  // Logic to get all cards
+};
+
+const createCard = async (cardData) => {
+  // Logic to create a new card
+};
+
+const updateCard = async (cardId, updatedData) => {
+  // Logic to update an existing card
+};
+
+const deleteCard = async (cardId) => {
+  // Logic to delete a card
+};
