ab9a29c21584756706b688aa17058f55e58f368e
Dossman-thomas <tdossman.development@gmail.com>
2025-09-11 08:07:51 -0400

    drafted CRUD operations to create, read, update, and delete flashcards from local storage for now

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -1,17 +1,30 @@
 // Crud Service functions for Getting, Creating, Updating, and Deleting flashcards
+const STORAGE_KEY = "flashcards";
 
 const getCards = async () => {
-  // Logic to get all cards
+  const cards = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+  return cards;
 };
 
 const createCard = async (cardData) => {
-  // Logic to create a new card
+  const cards = await getCards();
+  const newCard = { id: Date.now(), ...cardData };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify([...cards, newCard]));
+  return newCard;
 };
 
 const updateCard = async (cardId, updatedData) => {
-  // Logic to update an existing card
+  const cards = await getCards();
+  const updatedCards = cards.map((card) =>
+    card.id === cardId ? { ...card, ...updatedData } : card
+  );
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedCards));
+  return updatedCards.find((card) => card.id === cardId);
 };
 
 const deleteCard = async (cardId) => {
-  // Logic to delete a card
+  const cards = await getCards();
+  const updatedCards = cards.filter((card) => card.id !== cardId);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedCards));
+  return cardId;
 };
