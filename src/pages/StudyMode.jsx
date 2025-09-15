db0fc0742d85591643780b56ad91acf9f548eb50
Dossman-thomas <tdossman.development@gmail.com>
2025-09-15 12:06:56 -0400

    flashcards rendering data from local storage on page correctly, still needs previous/next, and flip animation.

--- src/pages/StudyMode.jsx
+++ src/pages/StudyMode.jsx
@@ -1,10 +1,32 @@
 import Button from "react-bootstrap/Button";
 import FlashCard from "../components/FlashCard";
+import { getCards } from "../services/cardService";
+import { useEffect, useState } from "react";
 
 export default function StudyPage() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      const storedCards = await getCards();
+      setCards(storedCards);
+    };
+    fetchCards();
+  }, []);
+
   return (
-    <div className="container-fluid centered">
-      <FlashCard question="What is React?" answer="A JavaScript library for building user interfaces. that uses JSX, a syntax extension that allows mixing HTML with JavaScript, and enables developers to create reusable UI components. It allows for the creation of single-page applications with a smooth user experience." />
+    <div className="container-fluid centered flex-column gap-3 mt-5">
+      {cards.length > 0 ? (
+        cards.map((card, index) => (
+          <FlashCard
+            key={index}
+            question={card.question}
+            answer={card.answer}
+          />
+        ))
+      ) : (
+        <p>No flashcards found! Add some first to begin studying!</p>
+      )}
     </div>
   );
 }
