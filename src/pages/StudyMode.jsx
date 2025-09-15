c8252cd1508e776b84e5e68fccad8291a9ad6057
Dossman-thomas <tdossman.development@gmail.com>
2025-09-15 10:42:55 -0400

    solid flashcard css implementation complete.  Cards designed to look like real flashcards/cuecards!

--- src/pages/StudyMode.jsx
+++ src/pages/StudyMode.jsx
@@ -4,7 +4,7 @@ import FlashCard from "../components/FlashCard";
 export default function StudyPage() {
   return (
     <div className="container-fluid centered">
-      <FlashCard question="What is React?" answer="A JavaScript library for building user interfaces. that uses JSX, a syntax extension that allows mixing HTML with JavaScript, and enables developers to create reusable UI components." />
+      <FlashCard question="What is React?" answer="A JavaScript library for building user interfaces. that uses JSX, a syntax extension that allows mixing HTML with JavaScript, and enables developers to create reusable UI components. It allows for the creation of single-page applications with a smooth user experience." />
     </div>
   );
 }
