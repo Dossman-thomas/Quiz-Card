5709d30e94890dd3fea53216cf17b4674aa381af
Dossman-thomas <tdossman.development@gmail.com>
2025-09-16 10:02:57 -0400

    flashcard flip animation fully functional and dialed in!

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -5,21 +5,23 @@ import "../styles/flashCard.css";
 export default function FlashCard({ question, answer }) {
   const [isFlipped, setIsFlipped] = useState(false);
 
+  const toggleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <div className="flashcard" onClick={() => setIsFlipped(!isFlipped)}>
-      {/* FRONT SIDE */}
-      {!isFlipped ? (
-        <div className="flashcard-front">
-          <p className="question-text">{question}</p>
-        </div>
-      ) : (
-        /* BACK SIDE */
+    <div
+      className={`flashcard ${isFlipped ? "is-flipped" : ""}`}
+      onClick={toggleFlip}
+    >
+      <div className="flashcard-inner">
+        <div className="flashcard-front">{question}</div>
         <div className="flashcard-back">
           <div className="notebook-lines">
             <p className="p-3 m-0">{answer}</p>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
