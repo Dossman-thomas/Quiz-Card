5a0269f025c5345282f4e89b59276411c7c5402f
Dossman-thomas <tdossman.development@gmail.com>
2025-09-19 09:44:20 -0400

    landing page fully styled

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,10 +3,10 @@ import Button from 'react-bootstrap/Button';
 export default function LandingPage () {
 
     return (
-        <div className='container-fluid centered flex-column'>
-            <h1>Welcome to QuizCard!</h1>
-            <h4>Ready to study, buddy?</h4>
-            <Button variant='primary' size='md' href='/manage-cards'>Let's Go!</Button>
+        <div className='container-fluid centered flex-column gap-2'>
+            <h1 className='m-0'>Welcome to QuizCard!</h1>
+            <h4 className='secondary-text mb-1'>Ready to study, buddy?</h4>
+            <Button className='primary-button' size='md' href='/manage-cards'>Let's Go!</Button>
         </div>
     );
 }
